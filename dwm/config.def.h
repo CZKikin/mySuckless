a8c6d53da7d2aa28de5b6c2998e32e18339f4f7a
CZKikin <krystofsadlik13@seznam.cz>
2020-12-27 17:00:11 +0000

    Updated rules

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -38,7 +38,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "WEB", "DIS", "SIG" };
 
 static const Rule rules[] = {
 		/* xprop(1):
@@ -47,12 +47,9 @@ static const Rule rules[] = {
 		 */
 		/* class      instance    title       tags mask     isfloating   monitor */
 		{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
-        { "discord",  NULL,       NULL,       1 << 1,       0,           -1 },
-        { "Steam",    NULL,       NULL,       1 << 2,       0,           -1 }, 
-        { "St",       NULL,  "neomutt",       1 << 4,       0,           -1 },
-        { "origin.exe", NULL,     NULL,       0,            1,           -1 },
-        { "Signal",   NULL,       NULL,       1 << 3,       0,           -1 },
-        { "Opera",    NULL,       NULL,       1 << 0,       0,           -1 },
+        { "discord",  NULL,       NULL,       1 << 7,       0,           -1 },
+        { "Signal",   NULL,       NULL,       1 << 8,       0,           -1 },
+        { "Opera",    NULL,       NULL,       1 << 6,       0,           -1 },
 };
 
 /* layout(s) */
