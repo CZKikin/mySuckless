9568c8df570a414b2ea15bd1fbbc7eb44b9da4ef
CZKikin <krystofsadlik13@seznam.cz>
2021-02-18 23:53:40 +0100

    Sync of config

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	{"", 		"~/.scripts/statusScripts/volume",          0,          		3},
 	{"", 		"~/.scripts/statusScripts/memory",	        10,         		1},
     {"", 		"~/.scripts/statusScripts/date",            1,                  2},
-    {"",        "~/.scripts/statusScripts/battery",         120,                4}
+    {"",        "~/.scripts/statusScripts/battery",         15,                 4}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
